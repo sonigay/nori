d9450b4f6673301b8b62963641783f4c88b16353
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-20 19:22:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print("----------------")
-	await client.change_presence(game=discord.Game(name='재고현황 안내', type=1))
+	await client.change_presence(game=discord.Game(name='업무외지원', type=1))
 
 
 
@@ -35,34 +35,34 @@ async def on_message(message):
 	global creds	#정산
     
           
-	if message.content.startswith('!재고'):
-		SearchID = message.content[len('!재고')+1:]
-		gc = gspread.authorize(creds)
-		wks = gc.open('오전재고').worksheet('시트1')
-		
-		wks.update_acell('A1', SearchID)
-		result = wks.acell('B1').value
-            
-		embed = discord.Embed(
-			title = ' :calling:  ' + SearchID + ' 재고현황! ',
-			description= '```' + SearchID + ' 오전까지 내역입니다. ' + result + '실시간조회가 아니라서 다소 차이가 있을수 있습니다. ```',
-			color=0xff00ff
-			)
-		await client.send_message(message.channel, embed=embed)
-            
-	if message.content.startswith('!모델명'):
-		SearchID = message.content[len('!모델명')+1:]
-		gc = gspread.authorize(creds)
-		wks = gc.open('오전재고').worksheet('시트2')
-		wks.update_acell('A1', SearchID)
-		result = wks.acell('B1').value
-		
-		embed = discord.Embed(
-			title = ' :printer:  모델명 코드 리스트 ',
-			description= '```' + SearchID + ' 모델명 코드는 ' + result + ' ```',
-			color=0x0000ff
-			)
-		await client.send_message(message.channel, embed=embed)
+#	if message.content.startswith('!재고'):
+#		SearchID = message.content[len('!재고')+1:]
+#		gc = gspread.authorize(creds)
+#		wks = gc.open('오전재고').worksheet('시트1')
+#		
+#		wks.update_acell('A1', SearchID)
+#		result = wks.acell('B1').value
+ #           
+#		embed = discord.Embed(
+#			title = ' :calling:  ' + SearchID + ' 재고현황! ',
+#			description= '```' + SearchID + ' 오전까지 내역입니다. ' + result + '실시간조회가 아니라서 다소 차이가 있을수 있습니다. ```',
+#			color=0xff00ff
+#			)
+#		await client.send_message(message.channel, embed=embed)
+ #           
+#	if message.content.startswith('!모델명'):
+#		SearchID = message.content[len('!모델명')+1:]
+#		gc = gspread.authorize(creds)
+#		wks = gc.open('오전재고').worksheet('시트2')
+#		wks.update_acell('A1', SearchID)
+#		result = wks.acell('B1').value
+#		
+#		embed = discord.Embed(
+#			title = ' :printer:  모델명 코드 리스트 ',
+#			description= '```' + SearchID + ' 모델명 코드는 ' + result + ' ```',
+#			color=0x0000ff
+#			)
+#		await client.send_message(message.channel, embed=embed)
                         
 access_token = os.environ["BOT_TOKEN"]
 git_access_token = os.environ["GIT_TOKEN"]
