54bc1c83eb76bb55fcb6d4ff8571bdc055985d53
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-20 19:43:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -34,7 +34,6 @@ async def on_message(message):
 	global gc #정산
 	global creds	#정산
     
-          
 #	if message.content.startswith('!재고'):
 #		SearchID = message.content[len('!재고')+1:]
 #		gc = gspread.authorize(creds)
@@ -63,7 +62,68 @@ async def on_message(message):
 #			color=0x0000ff
 #			)
 #		await client.send_message(message.channel, embed=embed)
-                        
+
+
+
+	if message.content.startswith('!주사위'):
+		randomNum = random.randrange(1, 7) # 1~6까지 랜덤수
+		print(randomNum)
+		if randomNum == 1:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: '+ ':one:'))
+		if randomNum == 2:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':two:'))
+		if randomNum ==3:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':three:'))
+		if randomNum ==4:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':four:'))
+		if randomNum ==5:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':five:'))
+		if randomNum ==6:
+			await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':six: '))
+			
+			
+			
+			
+	if message.content.startswith("!복권"):
+		Text = ""
+		number = [1, 2, 3, 4, 5, 6, 7]
+		count = 0
+		for i in range(0, 7):
+			num = random.randrange(1, 46)
+			number[i] = num
+			if count >= 1:
+				for i2 in range(0, i):
+					if number[i] == number[i2]:  # 만약 현재랜덤값이 이전숫자들과 값이 같다면
+						numberText = number[i]
+						print("작동 이전값 : " + str(numberText))
+						number[i] = random.randrange(1, 46)
+						numberText = number[i]
+						print("작동 현재값 : " + str(numberText))
+						if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
+							numberText = number[i]
+							print("작동 이전값 : " + str(numberText))
+							number[i] = random.randrange(1, 46)
+							numberText = number[i]
+							print("작동 현재값 : " + str(numberText))
+							if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
+								numberText = number[i]
+								print("작동 이전값 : " + str(numberText))
+								number[i] = random.randrange(1, 46)
+								numberText = number[i]
+								print("작동 현재값 : " + str(numberText))
+
+			count = count + 1
+			Text = Text + "  " + str(number[i])
+			
+		print(Text.strip())
+		embed = discord.Embed(
+			title="복권 숫자!",
+			description=Text.strip(),
+			colour=discord.Color.red()
+		)
+		await client.send_message(message.channel, embed=embed)
+			
+			
 access_token = os.environ["BOT_TOKEN"]
 git_access_token = os.environ["GIT_TOKEN"]
 client.run(access_token)
